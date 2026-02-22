48c826707f3dbea416652be2195196cd5b82c112
Levan Abashidze <abashidze.le1@gmail.com>
2026-02-22 21:40:46 +0400

    Navbar click

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
-menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
+// menuIcon.onclick = () => {
+//     navLinks.classList.toggle('active');
+// }
+
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+})
+
+document.addEventListener('click', e => {
+    if(!navLinks.contains(e.target) && e.target !== menuIcon) {
+        navLinks.classList.remove('active')
+    }
+})
+
